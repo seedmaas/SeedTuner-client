e54bb578114a7d263a4a2f4dcbdee16d9230abde
puremdq <dengquan.mu@se.com>
2023-11-11 22:06:23 +0800

    feat:客户端完善

--- socketio_client.py
+++ socketio_client.py
@@ -1,23 +1,45 @@
 import socketio
 
-# 创建一个Socket.IO客户端
 sio = socketio.Client()
 
-@sio.event
-def connect():
-    print("I'm connected!")
-    sio.emit('my_event', {'data': 'I\'m connected!'})
+token = 'token1'  # 假设你已经有了一个有效的token
 
-@sio.event
-def my_response(data):
-    print('Received message: ', data)
 
-@sio.event
-def disconnect():
-    print('I\'m disconnected')
+@sio.on('connect')
+def on_connect():
+    print('Connected to server with token {}'.format(token))
 
-# Connect to the server
-sio.connect('http://localhost:5000')
 
-# Keep the app running
-sio.wait()
+@sio.on('response_cpu_count')
+def on_request_cpu_count():
+    cpu_count = get_cpu_count()  # 假设你有一个函数可以获取CPU数量
+    print('CPU count requested, responding with count:', cpu_count)
+
+    # 发送CPU数量给服务器
+    sio.emit('response_cpu_count', {'id': token, 'count': cpu_count})
+
+
+@sio.on('disconnect')
+def on_disconnect():
+    print('Disconnected from server')
+
+
+def get_cpu_count():
+    import multiprocessing
+    return multiprocessing.cpu_count()  # 获取CPU数量的方法
+
+
+def connect_to_server():
+    print('Connecting to server...')
+    sio.connect('http://localhost:5000', headers={'token': token})
+
+
+if __name__ == '__main__':
+    connect_to_server()
+    on_request_cpu_count()
+    sio.wait()  # 阻塞进程，直到客户端断开连接
+
+
+
+# Note: To keep the connection alive, you should ensure that your Python script doesn't just exit.
+# You may have to do something to keep it running, such as entering an infinite loop or waiting for user input.
